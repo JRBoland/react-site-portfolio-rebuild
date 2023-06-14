fa733cd84f45504a41a72c6273f64225b3591c0c
JRBoland <james.boland94@hotmail.com>
2023-06-14 17:14:01 +1000

    added live version and repo links on projecs post page

--- src/pages/projectsPostPage.js
+++ src/pages/projectsPostPage.js
@@ -10,7 +10,8 @@ import blogImage6 from '../images/blog-image-6.jpg'
 import Aos from "aos"
 import "aos/dist/aos.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faArrowLeft, faFolderOpen, faLaptopCode } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight, faArrowLeft, faFolderOpen, faLaptopCode  } from '@fortawesome/free-solid-svg-icons'
+import { faGitAlt } from '@fortawesome/free-brands-svg-icons'
 
 export  function ProjectsPost(props) {
   
@@ -23,7 +24,8 @@ export  function ProjectsPost(props) {
       image: `${blogImage6}`,
       description: "What I got up to before being in dev",
       contents: "I had a couple projects, travelled and worked on boats in croatia, worked on music stuff, attempted to make a small business which is still ongoing, but infrequent (creating podcast intros) scored a short film for a london film school which was presented to film festivals. I funded this whilst working for the government dealing with tax and superannation. You can catch me on spotify @ bondla",
-      link: ""
+      link: "",
+      live: ""
     },
     { id: 2, 
       title: "Odin recipes: First ever website", 
@@ -32,7 +34,8 @@ export  function ProjectsPost(props) {
       image: `${blogImage2}`,
       description: "First website built from following the odin project",
       contents: "The very first site I built and deployed when self-teaching with theodinproject.com. A simple receipes site with Homer having a little boogie. Try seeing where the link to my other work takes you.",
-      link: "https://jrboland.github.io/odin-recipes/"
+      link: "https://github.com/JRBoland/odin-recipes",
+      live: "https://jrboland.github.io/odin-recipes/"
     },
     { id: 3, 
       title: "Portfolio Website: Old Version", 
@@ -41,7 +44,8 @@ export  function ProjectsPost(props) {
       image: `${blogImage5}`,
       description: "An old portfolio site done as one of the first assignments at Coder Academy.",
       contents: "My second built and deployed website was a portfolio site, made for the second assignment with Coder Academy. A good introduction to site building and styling, and using Github. Static website built using HTML and CSS.  This current site is built using React, you can view its Github repo here: __.",
-      link: "https://github.com/JRBoland/JamesBoland_T1A2"
+      link: "https://github.com/JRBoland/JamesBoland_T1A2",
+      live: "https://dotslashjb.netlify.app"
     },
     { id: 4, 
       title: "Terminal Application: Dog Helper Feeding Shelter", 
@@ -50,7 +54,8 @@ export  function ProjectsPost(props) {
       image: `${blogImage3}`,
       description: "Terminal CRUD application made with python",
       contents: "Intro to programming assignment, hard to come up with idea, landed on dog shelter feeding helper - idea is that you can add and remove dogs to a shelter 'inventory' mark which ones have been fed for the day and also monitor and edit their dietary needs. CRUD functionality within python, could do with a lot of refactoring as first project and code is messy.",
-      link: "https://github.com/JRBoland/T1A3-Terminal-Application"
+      link: "https://github.com/JRBoland/T1A3-Terminal-Application",
+      live: ""
     },
     { id: 5, 
       title: "Drone Logs - Flask API Web Server", 
@@ -59,7 +64,8 @@ export  function ProjectsPost(props) {
       image: `${blogImage4}`,
       description: "Drone logs API built with flask, python",
       contents: "Coder Academy assignment, built an API. Experience with flask framework, database relationship models, get requests, using insomnia and postman",
-      link: "https://github.com/JRBoland/JamesBoland_T2A2"
+      link: "https://github.com/JRBoland/JamesBoland_T2A2",
+      live: ""
     },
     { id: 6, 
       title: "Hackathon: GP Weather Clock", 
@@ -68,7 +74,8 @@ export  function ProjectsPost(props) {
       image: `${blogImage1}`,
       description: "Basic weather clock app built with a team during an in-term Coder Academy Hackathon",
       contents: "Learned git collaboration, further experience with react concepts, further use of HTML, CSS and responsive layout.",
-      link: "https://github.com/JRBoland/JamesBoland_T2A2"
+      link: "https://github.com/JRBoland/JamesBoland_T2A2",
+      live: "https://gpweatherclock.netlify.app/"
     },
   ]
 
@@ -118,7 +125,11 @@ export  function ProjectsPost(props) {
           <br/>
           <br/>
           {project.link && (
-            <a href={project.link} target="_blank" rel="noopener noreferrer" className="download-link-link"><FontAwesomeIcon icon={faLaptopCode} /> Link to the project</a>
+            <a href={project.link} target="_blank" rel="noopener noreferrer" className="download-link-link"><FontAwesomeIcon icon={faGitAlt} /> Repository</a>
+          )}
+          <br/>
+          {project.live && (
+            <a href={project.live} target="_blank" rel="noopener noreferrer" className="download-link-link"><FontAwesomeIcon icon={faLaptopCode} /> Live</a>
           )}
           
           </p>
