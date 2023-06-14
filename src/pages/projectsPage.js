2b36f751d86e29357394bff81ed509ec5db5df43
JRBoland <james.boland94@hotmail.com>
2023-06-14 16:59:32 +1000

    updated projects posts pages with images, included link to project, adjusted nav alignment and positioning

--- src/pages/projectsPage.js
+++ src/pages/projectsPage.js
@@ -6,6 +6,7 @@ import blogImage2 from '../images/blog-image-2.jpg'
 import blogImage3 from '../images/blog-image-3.jpg'
 import blogImage4 from '../images/blog-image-4.jpg'
 import blogImage5 from '../images/blog-image-5.jpg'
+import blogImage6 from '../images/blog-image-6.jpg'
 import Aos from "aos"
 import "aos/dist/aos.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -25,9 +26,10 @@ export  function Projects(props) {
       title: "before dev", 
       //post-image: ,
       date: "November 2020",
-      image: `${blogImage1}`,
+      image: `${blogImage6}`,
       description: "What I got up to before being in dev",
-      contents: "I had a couple projects, travelled and worked on boats in croatia, worked on music stuff, attempted to make a small business which is still ongoing, but infrequent (creating podcast intros) scored a short film for a london film school which was presented to film festivals. I funded this whilst working for the government dealing with tax and superannation. You can catch me on spotify @ bondla"
+      contents: "I had a couple projects, travelled and worked on boats in croatia, worked on music stuff, attempted to make a small business which is still ongoing, but infrequent (creating podcast intros) scored a short film for a london film school which was presented to film festivals. I funded this whilst working for the government dealing with tax and superannation. You can catch me on spotify @ bondla",
+      link: ""
     },
     { id: 2, 
       title: "Odin recipes: First ever website", 
@@ -35,39 +37,44 @@ export  function Projects(props) {
       date: "April 2022",
       image: `${blogImage2}`,
       description: "First website built from following the odin project",
-      contents: "The very first site I built and deployed when self-teaching with theodinproject.com. A simple receipes site with Homer having a little boogie. Try seeing where the link to my other work takes you."
+      contents: "The very first site I built and deployed when self-teaching with theodinproject.com. A simple receipes site with Homer having a little boogie. Try seeing where the link to my other work takes you.",
+      link: "https://jrboland.github.io/odin-recipes/"
     },
     { id: 3, 
       title: "Portfolio Website: Old Version", 
       //post-image: ,
       date: "November 2022",
-      image: `${blogImage3}`,
+      image: `${blogImage5}`,
       description: "An old portfolio site done as one of the first assignments at Coder Academy.",
-      contents: "My second built and deployed website was a portfolio site, made for the second assignment with Coder Academy. A good introduction to site building and styling, and using Github. Static website built using HTML and CSS.  This current site is built using React, you can view its Github repo here: __."
+      contents: "My second built and deployed website was a portfolio site, made for the second assignment with Coder Academy. A good introduction to site building and styling, and using Github. Static website built using HTML and CSS.  This current site is built using React, you can view its Github repo here: __.",
+      link: "https://github.com/JRBoland/JamesBoland_T1A2"
     },
     { id: 4, 
       title: "Terminal Application: Dog Helper Feeding Shelter", 
       //post-image: ,
       date: "December 2023",
-      image: `${blogImage4}`,
+      image: `${blogImage3}`,
       description: "Terminal CRUD application made with python",
-      contents: "Intro to programming assignment, hard to come up with idea, landed on dog shelter feeding helper - idea is that you can add and remove dogs to a shelter 'inventory' mark which ones have been fed for the day and also monitor and edit their dietary needs. CRUD functionality within python, could do with a lot of refactoring as first project and code is messy."
+      contents: "Intro to programming assignment, hard to come up with idea, landed on dog shelter feeding helper - idea is that you can add and remove dogs to a shelter 'inventory' mark which ones have been fed for the day and also monitor and edit their dietary needs. CRUD functionality within python, could do with a lot of refactoring as first project and code is messy.",
+      link: "https://github.com/JRBoland/T1A3-Terminal-Application"
     },
     { id: 5, 
       title: "Drone Logs - Flask API Web Server", 
       //post-image: ,
       date: "February 2023",
-      image: `${blogImage5}`,
+      image: `${blogImage4}`,
       description: "Drone logs API built with flask, python",
-      contents: "Coder Academy assignment, built an API. Experience with flask framework, database relationship models, get requests, using insomnia and postman"
+      contents: "Coder Academy assignment, built an API. Experience with flask framework, database relationship models, get requests, using insomnia and postman",
+      link: "https://github.com/JRBoland/JamesBoland_T2A2"
     },
     { id: 6, 
       title: "Hackathon: GP Weather Clock", 
       //post-image: ,
       date: "May 2023",
-      image: `${blogImage5}`,
+      image: `${blogImage1}`,
       description: "Basic weather clock app built with a team during an in-term Coder Academy Hackathon",
-      contents: "Learned git collarboation, further experience with react concepts, further use of HTML, CSS and responsive layout."
+      contents: "Learned git collaboration, further experience with react concepts, further use of HTML, CSS and responsive layout.",
+      link: "https://github.com/JRBoland/JamesBoland_T2A2"
     },
   ]
   projects.reverse()
@@ -78,6 +85,7 @@ export  function Projects(props) {
       <h1 className="h2-fa-icon"><span>Projects</span> <FontAwesomeIcon icon={faDiagramProject} className="h2-fa-icon-dec"/></h1>
       <p>
         These are some projects I've worked on during my short journey into web development.
+        **ALL PROJECT CONTENTS AND DESCRIPTIONS ARE CURRENTLY DRAFTS**
       </p>  
       <div className="projects-list">
       
--- src/pages/projectsPostPage.js
+++ src/pages/projectsPostPage.js
@@ -6,10 +6,11 @@ import blogImage2 from '../images/blog-image-2.jpg'
 import blogImage3 from '../images/blog-image-3.jpg'
 import blogImage4 from '../images/blog-image-4.jpg'
 import blogImage5 from '../images/blog-image-5.jpg'
+import blogImage6 from '../images/blog-image-6.jpg'
 import Aos from "aos"
 import "aos/dist/aos.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faArrowLeft, faRotateLeft } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight, faArrowLeft, faFolderOpen, faLaptopCode } from '@fortawesome/free-solid-svg-icons'
 
 export  function ProjectsPost(props) {
   
@@ -19,9 +20,10 @@ export  function ProjectsPost(props) {
       title: "before dev", 
       //post-image: ,
       date: "November 2020",
-      image: `${blogImage1}`,
+      image: `${blogImage6}`,
       description: "What I got up to before being in dev",
-      contents: "I had a couple projects, travelled and worked on boats in croatia, worked on music stuff, attempted to make a small business which is still ongoing, but infrequent (creating podcast intros) scored a short film for a london film school which was presented to film festivals. I funded this whilst working for the government dealing with tax and superannation. You can catch me on spotify @ bondla"
+      contents: "I had a couple projects, travelled and worked on boats in croatia, worked on music stuff, attempted to make a small business which is still ongoing, but infrequent (creating podcast intros) scored a short film for a london film school which was presented to film festivals. I funded this whilst working for the government dealing with tax and superannation. You can catch me on spotify @ bondla",
+      link: ""
     },
     { id: 2, 
       title: "Odin recipes: First ever website", 
@@ -29,39 +31,44 @@ export  function ProjectsPost(props) {
       date: "April 2022",
       image: `${blogImage2}`,
       description: "First website built from following the odin project",
-      contents: "The very first site I built and deployed when self-teaching with theodinproject.com. A simple receipes site with Homer having a little boogie. Try seeing where the link to my other work takes you."
+      contents: "The very first site I built and deployed when self-teaching with theodinproject.com. A simple receipes site with Homer having a little boogie. Try seeing where the link to my other work takes you.",
+      link: "https://jrboland.github.io/odin-recipes/"
     },
     { id: 3, 
       title: "Portfolio Website: Old Version", 
       //post-image: ,
       date: "November 2022",
-      image: `${blogImage3}`,
+      image: `${blogImage5}`,
       description: "An old portfolio site done as one of the first assignments at Coder Academy.",
-      contents: "My second built and deployed website was a portfolio site, made for the second assignment with Coder Academy. A good introduction to site building and styling, and using Github. Static website built using HTML and CSS.  This current site is built using React, you can view its Github repo here: __."
+      contents: "My second built and deployed website was a portfolio site, made for the second assignment with Coder Academy. A good introduction to site building and styling, and using Github. Static website built using HTML and CSS.  This current site is built using React, you can view its Github repo here: __.",
+      link: "https://github.com/JRBoland/JamesBoland_T1A2"
     },
     { id: 4, 
       title: "Terminal Application: Dog Helper Feeding Shelter", 
       //post-image: ,
       date: "December 2023",
-      image: `${blogImage4}`,
+      image: `${blogImage3}`,
       description: "Terminal CRUD application made with python",
-      contents: "Intro to programming assignment, hard to come up with idea, landed on dog shelter feeding helper - idea is that you can add and remove dogs to a shelter 'inventory' mark which ones have been fed for the day and also monitor and edit their dietary needs. CRUD functionality within python, could do with a lot of refactoring as first project and code is messy."
+      contents: "Intro to programming assignment, hard to come up with idea, landed on dog shelter feeding helper - idea is that you can add and remove dogs to a shelter 'inventory' mark which ones have been fed for the day and also monitor and edit their dietary needs. CRUD functionality within python, could do with a lot of refactoring as first project and code is messy.",
+      link: "https://github.com/JRBoland/T1A3-Terminal-Application"
     },
     { id: 5, 
       title: "Drone Logs - Flask API Web Server", 
       //post-image: ,
       date: "February 2023",
-      image: `${blogImage5}`,
+      image: `${blogImage4}`,
       description: "Drone logs API built with flask, python",
-      contents: "Coder Academy assignment, built an API. Experience with flask framework, database relationship models, get requests, using insomnia and postman"
+      contents: "Coder Academy assignment, built an API. Experience with flask framework, database relationship models, get requests, using insomnia and postman",
+      link: "https://github.com/JRBoland/JamesBoland_T2A2"
     },
     { id: 6, 
       title: "Hackathon: GP Weather Clock", 
       //post-image: ,
       date: "May 2023",
-      image: `${blogImage5}`,
+      image: `${blogImage1}`,
       description: "Basic weather clock app built with a team during an in-term Coder Academy Hackathon",
-      contents: "Learned git collarboation, further experience with react concepts, further use of HTML, CSS and responsive layout."
+      contents: "Learned git collaboration, further experience with react concepts, further use of HTML, CSS and responsive layout.",
+      link: "https://github.com/JRBoland/JamesBoland_T2A2"
     },
   ]
 
@@ -104,26 +111,36 @@ export  function ProjectsPost(props) {
         <p className="date" data-aos="fade-in">{project.date}</p>
         <div className="project-navigation">
         <Link to="/projects" className="project-navigation-links">
-          <p><FontAwesomeIcon icon={faRotateLeft} /> back to projects</p>
+          <p><FontAwesomeIcon icon={faFolderOpen} /> back to projects</p>
           </Link>
-         {nextProjectIndex !== null && (
-           <Link to={`/projects/${projects[nextProjectIndex].id}`} className="project-navigation-links">
-              <p>next project <FontAwesomeIcon icon={faArrowRight} /> </p>
-           </Link>
+          </div>
+          <p data-aos="fade-in">{project.contents}
+          <br/>
+          <br/>
+          {project.link && (
+            <a href={project.link} target="_blank" rel="noopener noreferrer" className="download-link-link"><FontAwesomeIcon icon={faLaptopCode} /> Link to the project</a>
           )}
+          
+          </p>
+          <br/>
+          <hr/>
+          <br/>
+          
+          </div>
+          <div className="project-nav">
           {previousProjectIndex !== null && (
-           <Link to={`/projects/${projects[previousProjectIndex].id}`} className="project-navigation-links">
+           <Link to={`/projects/${projects[previousProjectIndex].id}`} className="project-navigation-links right">
             <p><FontAwesomeIcon icon={faArrowLeft} /> previous project</p>
             </Link>
          )}
-         
-
+         {nextProjectIndex !== null && (
+           <Link to={`/projects/${projects[nextProjectIndex].id}`} className="project-navigation-links left">
+              <p>next project <FontAwesomeIcon icon={faArrowRight} /> </p>
+           </Link>
+          )}
+          </div>
         </div>
-      </div>
-     
-      <br/>
-      <p data-aos="fade-in">{project.contents}</p>
-      </div>
+      
       </main>
     </div>
   )
